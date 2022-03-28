ad658c1feda7db20fc88e5b19d68779aef16bbd7
anggamaulanasaputra <101842752+anggamaulanasaputra@users.noreply.github.com>
2022-03-28 19:51:04 +0700

    Add files via upload

---
+++ Biodata_1.php
@@ -0,0 +1,10 @@
+<?php
+class Biodata_1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama saya angga maulana saputra, saya saat ini aktif menjadi mahasiswa di universitas BSI Jurusan sistem informasi";
+    }
+    
+}
